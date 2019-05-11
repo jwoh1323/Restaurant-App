e09d8b85bf3dfef3c2057ca3315837d2c68e0768
dxvo <dxvo@uh.edu>
2019-05-11 11:25:56 -0500

    update

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import pandas as pd
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template('index.html')
 
-
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
