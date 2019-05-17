4c7d33e898b93e6eea286ce71dbdbc70ed3b10ae
dxvo <dxvo@uh.edu>
2019-05-16 19:06:55 -0500

    update

--- static/js/menu.js
+++ static/js/menu.js
@@ -54,14 +54,11 @@ function purchaseClicked() {
         dict[item_id] = [item_name,item_quality];
         cartItems.removeChild(cartItems.firstChild)
     }
-    updateCartTotal()
+    updateCartTotal();
         // POST to server
         // fetch('/https://ricerest.herokuapp.com/cart'
-<<<<<<< HEAD
+
     fetch('/https://ricerest.herokuapp.com/cart', {
-=======
-    fetch('https://ricerest.herokuapp.com/cart', {
->>>>>>> e9788a90dcaf90fc656427e2932ed4a45acc6892
         method: 'POST',
         body: JSON.stringify(dict)
     }).then(function (response) {
